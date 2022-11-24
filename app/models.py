e9fa00a1afd9327b094ecf965e192a07cda8387a
Zoe <ncr7804@naver.com>
2022-11-24 18:01:43 +0900

    post & food 합병, mysql 연동

--- app/models.py
+++ app/models.py
@@ -8,34 +8,21 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
+    username = Column(String(50), unique=True, index=True)
+    email = Column(String(50), unique=True, index=True)
+    hashed_password = Column(String(100))
     is_active = Column(Boolean, default=True)
 
     foods = relationship("Food", back_populates="owner")
-    posts =relationship("Post", back_populates="owner")
 
 class Food(Base):
     __tablename__ = "food"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    description = Column(String, index=True)
-    img_url=Column(String, index=True, nullable=True)
+    title = Column(String(50), index=True)
+    description = Column(String(200), index=True)
+    img_url=Column(String(50), index=True, nullable=True)
     owner_id = Column(Integer, ForeignKey("users.id"))
     created_dt=Column(DateTime, default=datetime.datetime.now())
 
     owner = relationship("User", back_populates="foods")
-
-class Post(Base):
-    __tablename__ = "posts"
-
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    img_url=Column(String, index=True, nullable=True)
-    description = Column(String, index=True)
-    created_dt=Column(DateTime, default=datetime.datetime.now())
-    owner_id = Column(Integer, ForeignKey("users.id"))
-
-    owner = relationship("User", back_populates="posts")
--- app/router/food_router.py
+++ app/router/food_router.py
@@ -1,20 +1,46 @@
+
 from sqlalchemy.orm import Session
-from fastapi import Depends, Request, UploadFile, File
+from fastapi import Depends, Request, UploadFile, Response
+from fastapi.responses import FileResponse
+from ..crud import food_crud
+from ..schemas import food_schemas
+from ..utils.auth_utils import AuthProvider
+from ..db.database import get_db
+
 from ..crud import food_crud
 from ..utils.auth_utils import AuthProvider
 from ..db.database import get_db, IMG_URL
-from typing import List
 import os
-from ..schemas.food_schemas import FoodBase
-
 from fastapi import APIRouter
+from.. import models
 
 router = APIRouter()
-# @router.get("/food")
-# def retrieve_post(reqeust: Request, db: Session = Depends(get_db)):
-#     return post_crud.list_post(request=reqeust,db=db)
+@router.get("")
+def retrieve_food(reqeust: Request, db: Session = Depends(get_db)):
+    return food_crud.list_food(request=reqeust,db=db)
 
+@router.post("")
+def creat_food(request: Request, food: food_schemas.foodBase, db: Session = Depends(get_db), dependencies=Depends(AuthProvider())):
+    return food_crud.create_food(request=request, db=db, food=food)
 
+@router.get("/me")
+def view_myfood(request: Request, db: Session = Depends(get_db), dependencies=Depends(AuthProvider())):
+    return food_crud.get_myfood(request=request, db=db)
 
+@router.get("/{food_id}")
+def retrieve_food(food_id: int, reqeust: Request, db: Session = Depends(get_db)):
+    return food_crud.retrieve_food(request=reqeust,db=db, food_id=food_id)
+
+@router.get("/{food_id}/img")
+async def upload_img(food_id: int, request: Request, db: Session = Depends(get_db), dependencies=Depends(AuthProvider())):
+    db_food=db.query(models.Food).filter(models.Food.id==food_id)
+    return FileResponse(os.path.join(IMG_URL, db_food[0].img_url))
+
+@router.post("/img")
+async def upload_img(request: Request, food_id: int, file: UploadFile, db: Session = Depends(get_db), dependencies=Depends(AuthProvider())):
    
+    contents = await file.read()
+    with open(os.path.join(IMG_URL, file.filename), "wb") as fp:
+        fp.write(contents)
+    return food_crud.update_img(request=request, db=db, food_id=food_id, img_url=file.filename)
     
