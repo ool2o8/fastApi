61d3a042cd2609c56553159b97ddf4921cd413b4
Zoe <ncr7804@naver.com>
2022-11-24 18:49:12 +0900

    fixed untracked files

--- app/db/database.py
+++ app/db/database.py
@@ -6,7 +6,7 @@ from pathlib import Path
 from .. import models
 
 BASE_DIR=Path(__file__).resolve().parent.parent
-secret_file = os.path.join(BASE_DIR, 'secret.json')
+secret_file = os.path.join(BASE_DIR, 'secrets.json')
 with open(secret_file) as f:
     secrets = json.loads(f.read())
 
